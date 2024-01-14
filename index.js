3ce6392ece4dabf99d7219ea0c5a1330c8c27691
shiko <sarah.tharao@student.moringaschool.com>
2024-01-14 18:49:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  initialize();
+});
+
+function initialize() {
+  fetchBooks();
+}
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
@@ -11,7 +21,3 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
